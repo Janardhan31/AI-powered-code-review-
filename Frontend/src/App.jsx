2db18743b8b13e040f65151eb41fe719baa0ad9e
Janardhan Kumar <janardhankr2003@gmail.com>
2025-05-05 16:59:29 +0530

    Add CORS support and integrate Axios for code review feature in frontend

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,30 +1,53 @@
 import { useState, useEffect, use } from "react";
 import "prismjs/themes/prism-tomorrow.css";
+import  Editor from "react-simple-code-editor";
 import Prism from "prismjs";
+import axios from "axios";
 import "./App.css";
+
 
 function App() {
   const [count, setCount] = useState(0);
+  const[code,setcode]=useState(` function sum(){
+  return 1+1
+}`)
+
+  const[review,setreview]=useState(``)
 
   useEffect(() => {
     Prism.highlightAll();
   });
+
+  async function reveiewcode(){
+    const response= await axios.post("http://localhost:3000/ai/get-review",{code})
+    setreview(response.data)
+  }
 
   return (
     <>
       <main>
         <div className="left">
           <div className="code">
-            <pre>
-              <code className="language-javascript">
-                {` function sum(){
-                return 1+1}`}
-              </code>
-            </pre>
+            <Editor
+              value={code}
+              onValueChange={(code) => setcode(code)}
+              highlight={code => Prism.highlight(code, Prism.languages.javascript, "javascript")}
+              padding={10}
+              style={{
+                fontFamily: '"Fira code", "Fira Mono", monospace',
+                fontSize: 12,
+                border: "1px solid #ddd",
+                borderRadius: "5px",
+                height: "100%",
+                width: "100%",
+              }}
+            />
           </div>
-          <div className="review">Review</div>
+          <div 
+          onClick={reveiewcode}
+          className="review">review</div>
         </div>
-        <div className="right"></div>
+        <div className="right">{review}</div>
       </main>
     </>
   );
