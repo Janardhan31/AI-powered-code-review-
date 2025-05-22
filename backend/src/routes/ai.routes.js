e0f7539ed3f44987e68834750f3d7dcebf86d3aa
Janardhan Kumar <janardhankr2003@gmail.com>
2025-05-22 12:50:46 +0530

    Auto commit on 05/22/2025 12:50:46

--- backend/src/routes/ai.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const aicontroller = require('../controllers/ai.controller');
-
-
-const router = express.Router();
-
-router.post("/get-review",aicontroller.getReview)
-
-module.exports = router;
